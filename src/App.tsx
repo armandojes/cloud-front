95f5ae7a087cc94d18263800f0b904432831013f
armandojes <armando@heroguest.com>
2024-06-02 16:28:27 -0600

    add config to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import environment from './config'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>AWS - CloudFront + Pipeline</h1>
+      <h1>AWS - CloudFront + Pipeline + {environment}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
